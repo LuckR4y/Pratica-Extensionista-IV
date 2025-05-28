89cc26f2cb88498a699e21d36cbf8c3cb4c29ac6
LuckR4y <137362549+LuckR4y@users.noreply.github.com>
2025-05-28 19:04:41 -0300

    Add files via upload

--- header.js
+++ header.js
@@ -12,3 +12,12 @@ window.addEventListener("scroll", () => {
 
   ultimoScroll = scrollAtual;
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  const logo = document.getElementById("logo-recarregar");
+  if (logo) {
+    logo.addEventListener("click", function () {
+      window.location.href = window.location.origin + window.location.pathname;
+    });
+  }
+});
